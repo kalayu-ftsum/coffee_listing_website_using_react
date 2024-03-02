e7de7df4a4fcbe9a0d6c389d2091b835aec599cc
Kalayu-Ftsum <kalayuftsumofficial@gmail.com>
2024-03-02 11:45:08 +0100

    added card elements

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import FilledStarSVG from '../assets/images/Star_fill.svg';
 import EmptyStarSVG from '../assets/images/Star.svg';
-export default function Card() {
+export default function Card({coffee:{
+  name,
+  price,
+  ratings,
+  votes,
+  isPopular
+}}) {
   return (
     <div className='img'>
-      Card
-   </div>
+    <img 
+    src='https://images.unsplash.com/photo-1585747733279-8f64c2b71381?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGF0dGV8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
+    alt="coffee -mug"
+    />
+    <div className='card_description'>
+    <span className='product_name'>{name}</span>
+    <span className='price_tag'>${price.toFixed(2)}</span>
+    <span className='rating_tag'>
+         <img src={ratings? FilledStarSVG: EmptyStarSVG} alt="My SVG" /> 
+         {ratings}
+         <span className="rating_count"> {ratings?`(${votes} votes)`: 'No ratings'}</span>
+    </span>
+    </div>
+    {isPopular&&<span className='popular'>popular</span>}
+</div>
   )
 }
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,16 +1,52 @@
 import React from 'react';
 import Card from './Card';
 
+const CoffeeList=[{
+    name:"Cappuccino",
+    price:5.20,
+    ratings:4.7,
+    votes:65,
+    isPopular:true
+},
+{
+    name:"House Coffee",
+    price:3.50,
+    ratings:4.85,
+    votes:14,
+    isPopular:true
+},
+{
+    name:"Espresso",
+    price:2.50,
+    ratings:4.9,
+    votes:55,
+    isPopular:false
+},
+{
+    name:"Coffee Latte",
+    price:4.50,
+    ratings:5.0,
+    votes:23,
+    isPopular:false
+},
+{
+    name:"Chocolate Coffee",
+    price:4.00,
+    ratings:4.65,
+    votes:122,
+    isPopular:false
+},
+{
+    name:"Valentine Special",
+    price:5.50,
+    isPopular:false
+}
+]
 export default function CardList() {
   return (<React.Fragment>
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    {
+        CoffeeList.map((coffee)=><Card coffee={coffee} key={coffee.name} />)
+    }
     </React.Fragment>
   )
 }
