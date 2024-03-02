cdc9376c43fdc1f302e1945f61abdd72ba3619a7
Kalayu-Ftsum <kalayuftsumofficial@gmail.com>
2024-03-02 11:31:05 +0100

    Added hero page

---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HeroImg from '../assets/images/bg-cafe.jpg';
+
+export default function Hero() {
+  return (
+    <div className='hero'>
+    <img src={HeroImg} alt="hero page"/>
+    </div>
+  )
+}
