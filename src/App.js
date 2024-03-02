3071edb2875c9aa9741d5c810c33c17222a67873
Kalayu-Ftsum <kalayuftsumofficial@gmail.com>
2024-03-02 11:43:42 +0100

    added style on main and main header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Hero from './components/Hero';
+import Main from './components/Main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Hero />
+        <Main />
       </div>
     );
   }
